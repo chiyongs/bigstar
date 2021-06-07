bfa27c6907c12a4499bfa6b3d984bbfdbf768539
chiyongs <tlscldyd@gmail.com>
2021-06-07 15:16:57 +0900

    프론트엔드와 합칠 준비

--- src/main/java/bibibig/bigstar/controller/FoodingController.java
+++ src/main/java/bibibig/bigstar/controller/FoodingController.java
@@ -2,6 +2,7 @@ package bibibig.bigstar.controller;
 
 import bibibig.bigstar.domain.Fooding;
 import bibibig.bigstar.domain.LikesByDate;
+import bibibig.bigstar.domain.LikesByFood;
 import bibibig.bigstar.repository.FoodingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -21,11 +23,18 @@ public class FoodingController {
     public String fooding (@RequestParam String foodName, Model model) {
 
         // 날짜에 따른 좋아요 수
-        List<LikesByDate> likesByDate = foodingRepository.getLikesByDate(foodName);
-        List<Fooding> foods = foodingRepository.findByFoodName(foodName);
-//        for (Fooding food : foods) {
-//            food.
-//        }
+        List<LikesByDate> likesByDates = foodingRepository.getLikesByDate(foodName);
+        List<Fooding> byFoodName = foodingRepository.findByFoodName(foodName);
+        List<LikesByFood> likesByFoods = foodingRepository.getLikesByFood();
+        List<Fooding> foods = new ArrayList<>();
+        for(int i=0; i<3; i++) {
+            foods.add(byFoodName.get(i));
+        }
+
+        model.addAttribute("likesByFoods", likesByFoods);
+        model.addAttribute("likesByDates",likesByDates);
+        // 해당 음식에 대해 좋아요 랭킹 3개
+        model.addAttribute("foods", foods);
 
         return "ok";
 
